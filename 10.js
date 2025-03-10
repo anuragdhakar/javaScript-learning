a68c122d7e0f6e764ef4e9d4913296edb1f3882c
anuragdhakar <anuragjarariya777@gmail.com>
2025-03-10 16:22:20 +0530

    10

---
+++ 10.js
@@ -0,0 +1,11 @@
+let btn=document.querySelector('#btn')
+btn.addEventListener('click',()=>{alert("hello")})
+
+let dbl=document.querySelector('#dbl')
+dbl.addEventListener('dblclick',()=>{alert("hello")})
+
+let enter=document.querySelector('#enter')
+enter.addEventListener('mouseenter',()=>{alert("hello")})
+
+let leave=document.querySelector('#leave')
+leave.addEventListener('mouseleave',()=>{alert("hello")})
---
+++ array.js
@@ -0,0 +1,11 @@
+//10-march-25---------------------------------------------------------------------------------------------------
+// Array- collection of different type of data
+//[],ordered 
+//index => start from 0( position of element )
+
+let arr=[2,'a',"anurag",1.11,true]
+// console.log(arr[2]);
+
+for(let vl of arr){
+    console.log(vl);
+}
