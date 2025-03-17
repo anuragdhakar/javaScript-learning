7f518fae8b1818bc2c9646b3f2a9962bb99e2005
anuragdhakar <anuragjarariya777@gmail.com>
2025-03-17 16:13:19 +0530

    10

--- array.js
+++ array.js
@@ -25,19 +25,50 @@
 //--------------------------------------------12-march-25----------------------------------------------------
 
 //To add last element
-let aar=[1,3,5,6]
-aar.push("last element")
-console.log(aar);
+//let aar=[1,3,5,6]
+// aar.push("last element")
+// console.log(aar);
 
-//to add element in front of array
-aar.unshift("first element","2")
-console.log(aar);
+// //to add element in front of array
+// aar.unshift("first element","2")
+// console.log(aar);
 
 
-//to delete element from the front
-aar.shift()
-console.log(aar);
+// //to delete element from the front
+// aar.shift()
+// console.log(aar);
 
-// to delete an element from the last of an array
-aar.pop()
-console.log(aar);
+// // to delete an element from the last of an array
+// aar.pop()
+// console.log(aar);
+//--------------------------------------------------------------------------------------------------------------
+// let data=["anurag",22,7987329834,"chetak bridge","FSD"]
+// let part=data.slice(1,4)
+// console.log(part);
+
+//ques even
+// let ar1= [1,2,3,45,6,7,8,89,10,34,76,87]
+// let one=ar1.slice(2,9);
+
+// for(let a of one){
+//     if(a%2==0){
+//         console.log(a)
+//     }
+// }
+//
+
+//------------------------------------------17-march-2025--------------------------------------------------------
+//splice method used to delete an element from the array and we also can add elements in an array 
+let arr=["cybrom",26,"milan",46]
+arr.splice(2,1,"anurag","patel")
+console.log(arr)
+
+//Destructuring of Array ---if we want to use the elements of an array as  variables known as destructuring of array.
+let [name,age,shop,ct]=["cybrom",26,"milan",46]
+console.log(shop)
+
+//(...)-rest operator--used in functions / spread operator-- merging of 2 array in a single array  
+let ar1=["cybrom",26,"milam",46]
+let ar2=[2,7,5,6]
+let ar3=[...ar1,...ar2]
+console.log(ar3)
